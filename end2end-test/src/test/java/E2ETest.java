38f1e5bf9649f67d3b206fd67e93db13062ad8fe
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-11-26 22:23:24 +0100

    go on

--- end2end-test/src/test/java/E2ETest.java
+++ end2end-test/src/test/java/E2ETest.java
@@ -1,30 +1,35 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.vertx.core.json.JsonObject;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.Matchers.*;
 
 public class E2ETest {
     @BeforeAll
     public static void setup() {
-        RestAssured.baseURI = "http://localhost:10000";  // Usa la variabile d'ambiente per il gateway
+        RestAssured.baseURI = "http://apigateway:10000";  // Usa la variabile d'ambiente per il gateway
     }
 
     @Test
     public void testUserJourney() {
         // Step 1: Creare un utente
-        String userId = "testUser123";
-        Response createUserResponse = RestAssured.given()
+        String userId = "testUser12345";
+        RestAssured.given()
             .contentType("application/json")
             .body(new JsonObject().put("userId", userId).encode())
             .post("/api/users")
             .then()
             .statusCode(200)
             .extract().response();
+
+        RestAssured.given()
+            .contentType("application/json")
+            .body(new JsonObject().put("userId", userId).put("credit", 1000).encode())
+            .post("/api/users/"+userId+"/recharge-credit")
+            .then()
+            .statusCode(200)
+            .extract().response();
         
-        String createdUserId = createUserResponse.jsonPath().getString("user.userId");
         // Step 2: Creare una bicicletta
         Response getBikeIds = RestAssured.given()
             .contentType("application/json")
